759aee2a63277c34bcf53844f92d9c1213829752
mohammad <akbarnezhadProgrammer@gmail.com>
2025-09-19 14:58:10 +0330

    mhd_user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userController;
+use App\Http\Middleware\checkAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::view('/home', 'home') -> name("home");
+Route::view('/notAccess', 'notAccess') -> name("notAccess");
+
+
+Route::controller(userController::class)->prefix("user") -> group(function (){
+
+    Route::get("/signup","signup")->name("user_signUp") ;
+    Route::post("/store","store") -> name("user_store");
+
+    Route::get("/login","login") -> name("user_login");
+    Route::post("/check_user","checkUser") -> name("checkUser");
+    Route::get("/profile/{id?}","profile") ->name("user_profile");
+    // Route::get("/index","index");
+    // Route::get("/edit/{id}","edit");
+    // Route::post("/updata","updata");
+    // Route::get("/delete/{id}","delete");
+
+});
+
+Route::controller(userController::class)->prefix("admin") -> group(function (){
+
+    Route::get("/signUpUser","signup")-> middleware(checkAdminMiddleware::class) ->name("admin_create_user") ;
+    // Route::post("/store","store") -> name("user_store");
+
+    // Route::get("/loginUser","login") -> name("user_login");
+    // Route::post("/check_login","checkLogin");
+    Route::get("/profile/{id?}","profile")-> middleware(checkAdminMiddleware::class);
+    // Route::get("/index","index");
+    // Route::get("/edit/{id}","edit");
+    // Route::post("/updata","updata");
+    // Route::get("/delete/{id}","delete");
+
 });
