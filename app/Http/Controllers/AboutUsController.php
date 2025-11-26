063b307e68ca5a0f92b919ac2190a6b0102fba17
Mohammad Eftekhari <turkaslan1206@gmail.com>
2025-11-26 16:10:44 +0330

    debug products images

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\aboutUs;
+use App\Models\category;
+use App\Models\footer_column;
+use App\Models\footerTile;
+use App\Models\logo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AboutUsController extends Controller
 {
@@ -12,6 +17,7 @@ class AboutUsController extends Controller
         $aboutus = aboutUs::all();
         return view('admin.aboutus.create', ['aboutus' => $aboutus]);
     }
+
     public function upsert(Request $request)
     {
         $aboutus = aboutUs::all();
@@ -23,21 +29,36 @@ class AboutUsController extends Controller
             return to_route('aboutus-index');
         }
     }
+
     public function store(Request $request)
     {
         $originalName = request()->image->getClientOriginalName();
         $path = $request->file('image')->storeAs('images', time() . $originalName, 'public');
         aboutUs::create([
             'description' => $request->description,
-            'image' => "storage/" . $path
+            'image' => 'storage/' . $path
         ]);
         return to_route('aboutus-index');
     }
+
     public function index()
     {
+        $cats = category::all();
+        $logo = logo::all();
+        $footer_columns = footer_column::whereIn('section_number', [1, 2, 3])->with('rows')->get();
+        $footer_form_column = footer_column::whereIn('section_number', [4])->with('images')->with('texts')->get();
+        $user = Auth::user();
         $aboutus = aboutUs::all();
-        return view('aboutus', ['aboutus' => $aboutus]);
+        return view('aboutus', [
+            'aboutus' => $aboutus,
+            'categories' => $cats,
+            'logo' => $logo,
+            'footerColumns' => $footer_columns,
+            'footer_form_column' => $footer_form_column,
+            'user' => $user
+        ]);
     }
+
     public function update(Request $request)
     {
         aboutUs::query()->delete();
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\category;
+use App\Models\footer_column;
+use App\Models\logo;
+use App\Models\settings;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\settings;
 
 class SearchController extends Controller
 {
@@ -13,6 +15,18 @@ class SearchController extends Controller
     {
         $settings = settings::all();
         $user = Auth::user();
-        return view('search', ['settings' => $settings, 'category' => $category, 'user' => $user]);
+        $cats = category::all();
+        $logo = logo::all();
+        $footer_columns = footer_column::whereIn('section_number', [1, 2, 3])->with('rows')->get();
+        $footer_form_column = footer_column::whereIn('section_number', [4])->with('images')->with('texts')->get();
+        return view('search', [
+            'settings' => $settings,
+            'category' => $category,
+            'user' => $user,
+            'categories' => $cats,
+            'logo' => $logo,
+            'footerColumns' => $footer_columns,
+            'footer_form_column' => $footer_form_column,
+        ]);
     }
 }
