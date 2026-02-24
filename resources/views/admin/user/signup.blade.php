71149e9e579bbc091d41d09296d5f8f33239549c
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-02-24 22:05:17 +0330

    admin user create

--- resources/views/admin/user/signup.blade.php
+++ resources/views/admin/user/signup.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="fa" dir="rtl">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}" type="text/css">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>signup</title>
-</head>
-
-<body>
-    @extends('admin.app.dashboard')
-    @section('content')
-    <main class="w-full h-svh bg-[url('https://keenthemes.com/static/metronic/tailwind/dist/assets/media/images/2600x1200/bg-10.png')] bg-cover bg-no-repeat flex flex-row justify-center items-center">
+@extends('admin.app.dashboard')
+@section('content')
+    {{-- <main class="w-full h-svh bg-[url('https://keenthemes.com/static/metronic/tailwind/dist/assets/media/images/2600x1200/bg-10.png')] bg-cover bg-no-repeat flex flex-row justify-center items-center">
         <div class="kt-card max-w-[370px] w-full">
             <form action="{{route('user.store')}}" method="post" class="flex flex-col gap-5 p-10 bg-white">
                 @csrf
@@ -71,8 +59,59 @@
                 <button class="py-1.5 rounded-md text-white text-sm font-bold bg-[#2b7fff] cursor-pointer hover:bg-[#2b7fff]/90">ثبت نام</button>
             </form>
         </div>
-    </main>
-    @endsection
-</body>
+    </main> --}}
 
-</html>
+    <div class="w-full h-full pb-10">
+        <h2 class="text-3xl text-center font-bold py-10 text-[#425A8B]">فرم ایجاد دسته بندی</h2>
+        <div class="lg:w-2/3 w-full mx-auto border border-[#D5DFE4] rounded-[10px] text-[#425A8B] p-5 bg-white">
+            <form action="{{ route('user.store') }}" method="post" enctype="multipart/form-data" class="w-full grid grid-cols-1 lg:grid-cols-2 gap-5">
+                @csrf
+                <div class="w-full flex flex-col">
+                    <label for="catTitle" class="mb-2 flex flex-row items-center">
+                        <span>
+                            نام :
+                            <span class="text-rose-500">*</span>
+                        </span>
+                    </label>
+                    <input type="text" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                        name="name" placeholder="نام را وارد کنید" required>
+                </div>
+                <div class="w-full flex flex-col">
+                    <label for="catTitle" class="mb-2 flex flex-row items-center">
+                        <span>
+                            نام خانوادگی :
+                            <span class="text-rose-500">*</span>
+                        </span>
+                    </label>
+                    <input type="text" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                        name="family" placeholder="نام خانوادگی را وارد کنید" required>
+                </div>
+                <div class="w-full flex flex-col">
+                    <label for="catTitle" class="mb-2 flex flex-row items-center">
+                        <span>
+                            شماره تلفن :
+                            <span class="text-rose-500">*</span>
+                        </span>
+                    </label>
+                    <input type="number" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                        name="phoneNumber" placeholder="0912345678" required>
+                </div>
+                <div class="w-full flex flex-col">
+                    <label for="catTitle" class="mb-2 flex flex-row items-center">
+                        <span>
+                            رمز عبور :
+                            <span class="text-rose-500">*</span>
+                        </span>
+                    </label>
+                    <input type="password" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                        name="password" required>
+                </div>
+        
+                <div class="mt-5 text-center lg:col-span-2">
+                    <button type="submit"
+                        class="py-3 px-10 rounded-[10px] bg-[#1B84FF] hover:bg-[#056EE9] text-white cursor-pointer">ثبت</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
