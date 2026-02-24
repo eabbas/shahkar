ff517ad74986f906ae4ecc16c3b0097a8146de9f
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-02-24 16:50:27 +0330

    course edit

--- resources/views/admin/course/edit.blade.php
+++ resources/views/admin/course/edit.blade.php
@@ -1,164 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    @extends('admin.app.dashboard')
-    @section('content')
-        <form action="{{ route('course.update') }}" enctype='multipart/form-data' method="POST">
-            @csrf
-            <div class="min-h-screen flex items-start justify-center">
-                <div class="bg-white rounded-2xl shadow-md p-3 w-full md:w-9/12">
-                    <!-- هدر -->
-                    <div class="text-center mb-4">
-                    </div class="w-full">
-                    <div class="md:flex md:flex-row md:w-full md:items-center md:gap-5">
-
-                        <div class="md:flex md:flex-col md:w-full">
-                            <input type="hidden" name="id" value="{{ $course->id }}">
-
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="kelass p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    عکس</legend>
-                                <input type="file" name='img'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500">
-                            </fieldset>
-
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="kelass p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    ویدیو</legend>
-                                <input type="file" name='video'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500">
-                            </fieldset>
-
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    عنوان</legend>
-                                <input type="text" name='title'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"
-                                    value="{{ $course->title }}">
-                            </fieldset>
-                        </div>
-
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    توضیحات</legend>
-                                <input type="text" name='description'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"
-                                    value="{{ $course->description }}">
-                            </fieldset>
-                        </div>
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    خلاصه</legend>
-                                <input type="text" name='summary'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"value="{{ $course->summary }}">
-                            </fieldset>
-                        </div>
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    قیمت</legend>
-                                <input type="text" name='price'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"value="{{ $course->price }}">
-                            </fieldset>
-                        </div>
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    مدت دوره</legend>
-                                <input type="text" name='duration'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"value="{{ $course->duration }}">
-                            </fieldset>
-                        </div>
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    رشد</legend>
-                                <input type="text" name='progress'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"value="{{ $course->progress }}">
-                            </fieldset>
-                        </div>
-                        <div class="md:flex md:flex-col md:w-full">
-                            <fieldset class="mt-2 text-sm md:text-base border border-gray-400 rounded-[15px] py-1 pr-3">
-                                <legend
-                                    class="p-1 w-30 bg-[#1cb7fd] text-white rounded-full flex flex-row justify-center text-sm">
-                                    تخفیف</legend>
-                                <input type="text" name='discount'
-                                    class="w-full px-2 py-1 md:px-2 outline-none text-gray-500"value="{{ $course->discount }}">
-                            </fieldset>
-                        </div>
-
-                    </div>
-                    <label for="free">free:</label>
-                    <input type="checkbox" name="free" value="1"
-                        @if ($course->free) {{ 'checked' }} @endif>
-
-
-                    <div>
-                        <label for="show_in_home">Showhome:</label>
-                        <input type="checkbox" name="homes" value="1"
-                            @if ($course->show_in_home) {{ 'checked' }} @endif>
+@extends('admin.app.dashboard')
+@section('title')
+    شاهکار | ویرایش دوره {{ $course->title }}
+@endsection
+@section('content')
+    <div class="w-full h-full pb-10">
+        <h2 class="text-3xl text-center font-bold py-10 text-[#425A8B]">ویرایش دوره {{ $course->title }}</h2>
+        <div class="lg:w-2/3 w-full mx-auto border border-[#D5DFE4] rounded-[10px] text-[#425A8B] p-5 bg-white">
+            <form action="{{ route('course.update') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="id" value="{{ $course->id }}">
+                <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5">
+                    <div class="w-full flex flex-col">
+                        <label for="catTitle" class="mb-2 flex flex-row items-center">
+                            <span>
+                                عنوان دوره:
+                                <span class="text-rose-500">*</span>
+                            </span>
+                        </label>
+                        <input type="text" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="title" id="catTitle" placeholder=" عنوان دوره را وارد کنید" required
+                            value="{{ $course->summary }}">
                     </div>
 
-                    <label for="category">category:</label>
-                    <select name="category_id">
-                        <option value="0">بدون والد</option>
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}"
-                                @if ($category->id == $course->category_id) {{ 'selected' }} @endif>{{ $category->title }}
-                            </option>
-                        @endforeach
-                    </select>
-                    <label for="teacher">teacher:</label>
-                    <select name="teacher_id">
-                        @foreach ($teachers as $teacher)
-                            <option value="{{ $teacher->id }}"
-                                @foreach ($course->teachers as $courseTeacher) @if ($teacher->id == $courseTeacher->id) {{ 'selected' }}  @endif @endforeach>
-                                {{ $teacher->name }}</option>
-                        @endforeach
-                    </select>
-                    <label for="level">level:</label>
-                    <select name="level_id">
-                        @foreach ($levels as $level)
-                            <option value="{{ $level->id }}"
-                                @if ($course->level_id == $level->id) {{ 'selected' }} @endif>{{ $level->title }}</option>
-                        @endforeach
-                    </select>
-                    <label for="status">status:</label>
-                    <select name="status_id">
-                        @foreach ($statuses as $status)
-                            <option value="{{ $status->id }}"
-                                @if ($course->status_id == $status->id) {{ 'selected' }} @endif>{{ $status->title }}</option>
-                        @endforeach
-                    </select>
+                    <div class="w-full flex flex-col">
+                        <label for="cat_desc" class="mb-2">خلاصه دوره:</label>
+                        <textarea rows="1" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            placeholder="خلاصه" name="summary">{{ $course->summary }}</textarea>
+                    </div>
+
+                    <div class="w-full flex flex-col">
+                        <label for="category_id">دسته بندی :</label>
+                        <select name="category_id" id="category_id" class="w-full bg-[#F9F9F9] py-3 pr-5 rounded-[10px]">
+                            <option value="0">بدون والد</option>
+                            @foreach ($categories as $category)
+                            <option value="{{$category->id}}" @if ($category->id == $course->category_id) {{ 'selected' }} @endif>{{$category->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="w-full flex flex-col">
+                        <label for="teacher">انتخاب استاد :</label>
+                        <select name="teacher" id="teacher" class="w-full bg-[#F9F9F9] py-3 pr-5 rounded-[10px]">
+                             @foreach ($teachers as $teacher)
+                            <option value="{{$teacher->id}}" @foreach ($course->teachers as $courseTeacher) @if ($teacher->id == $courseTeacher->id) {{ 'selected' }}  @endif @endforeach>{{$teacher->name}} {{ $teacher->family }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="w-full flex flex-col">
+                        <label for="level_id">انتخاب سطح :</label>
+                        <select name="level_id" id="level_id" class="w-full bg-[#F9F9F9] py-3 pr-5 rounded-[10px]">
+                             @foreach ($levels as $level)
+                            <option value="{{ $level->id }}"  @if ($course->level_id == $level->id) {{ 'selected' }} @endif>{{ $level->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="w-full flex flex-col">
+                        <label for="status_id">انتخاب وضعیت :</label>
+                        <select name="status_id" id="status_id" class="w-full bg-[#F9F9F9] py-3 pr-5 rounded-[10px]">
+                             @foreach ($statuses as $status)
+                            <option value="{{ $status->id }}"  @if ($course->status_id == $status->id) {{ 'selected' }} @endif>{{ $status->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">میزان رشد دوره:</label>
+                        <input type="number"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="progress" id="progress" value="{{ $course->progress }}">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">مدت دوره:</label>
+                        <input type="text"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="duration" id="duration" value="{{ $course->duration }}">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">قیمت دوره:</label>
+                        <input type="number"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="price" id="price" value="{{ $course->price }}">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">تخفیف دوره:</label>
+                        <input type="text"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="discount" id="discount" value="{{ $course->discount }}">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">تصویر دوره:</label>
+                        <input type="file"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="img" id="img">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="image" class="mb-2">ویدیو معرفی دوره:</label>
+                        <input type="file"
+                            class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            name="video" id="video">
+                    </div>
+                    <div class="w-full flex flex-row items-center gap-3 lg:gap-5">
+                        <label for="image" class="mb-2">رایگان :</label>
+                        <input type="checkbox" class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]" name="free" value="1" @if ($course->free) {{ 'checked' }} @endif>
+                    </div>
+                    <div class="w-full flex flex-row items-center gap-3 lg:gap-5">
+                        <label for="image" class="mb-2">نمایش در خانه :</label>
+                        <input type="checkbox" class="w-full outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]" name="homes" value="1" @if ($course->show_in_home) {{ 'checked' }} @endif>
+                    </div>
+                    <div class="w-full flex flex-col lg:col-span-2">
+                        <label for="cat_desc" class="mb-2">توضیحات دوره:</label>
+                        <textarea class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]" placeholder="توضیحات"
+                            name="description">{{ $course->description }}</textarea>
+                    </div>
                 </div>
-            </div>
-
-            <button type="submit"
-                class="active:bg-[#0080e5] mt-2 w-full bg-[#03A9F4] text-white py-3 rounded-md hover:bg-blue-700 transition duration-200 font-medium">
-                ارسال اطلاعات
-            </button>
-            </div>
-            </div>
-            </fieldset>
-        </form>
-    @endsection
-</body>
-
-</html>
+                <div class="text-center">
+                    <button type="submit"
+                        class="py-3 px-10 rounded-[10px] bg-[#1B84FF] hover:bg-[#056EE9] text-white cursor-pointer">ثبت</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
