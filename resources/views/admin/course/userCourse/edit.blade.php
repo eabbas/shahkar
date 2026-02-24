c5eb2b1c46df5f479f4e91874e3d1725a82d5186
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-02-24 17:52:21 +0330

    user course edit

--- resources/views/admin/course/userCourse/edit.blade.php
+++ resources/views/admin/course/userCourse/edit.blade.php
@@ -1,40 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    @extends('admin.app.dashboard')
-    @section('content')
-        <form action ="{{ route('userCourse.update') }}" method="POST">
-            @csrf
-            <div>
+@extends('admin.app.dashboard')
+@section('title')
+    شاهکار | ویرایش دوره کاربر
+@endsection
+@section('content')
+    <div class="w-full h-full pb-10">
+        <h2 class="text-3xl text-center font-bold py-10 text-[#425A8B]">فرم ویرایش دوره کاربر</h2>
+        <div class="lg:w-2/3 w-full mx-auto border border-[#D5DFE4] rounded-[10px] text-[#425A8B] p-5 bg-white">
+            <form action="{{ route('course.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
                 <input type ="hidden" id="id" name="id" value="{{ $userCourse->id }}">
-            </div>
-            <div>
-                <labl for="progress"> progress:</label>
-                    <input type ="text" id=" progress" name="progress" required value="{{ $userCourse->progress }}">
-            </div>
-            <div>
-                <labl for="status"> status:</label>
-                    <input type ="text" id=" status" name="status" required value="{{ $userCourse->status }}">
-            </div>
-            <div>
-                <labl for="course"> course:</label>
-                    <select name="course_id">
-                        @foreach ($courses as $course)
-                            <option value="{{ $course->id }}"
-                                @if ($course->id == $userCourse->course_id) {{ 'selected' }} @endif>{{ $course->title }}</option>
-                        @endforeach
-                    </select>
-            </div>
-            <button type="submit">edit</button>
-        </form>
-    @endsection
-</body>
-
-</html>
+                <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5">
+                    <div class="w-full flex flex-col">
+                        <label for="cat_desc" class="mb-2">پیشرفت :</label>
+                        <input type="text" class="outline-none pr-5 py-3 bg-[#F9F9F9] rounded-[12px] focus:bg-[#f1f1f4]"
+                            placeholder="روند پیشرفت" name="progress" required value="{{ $userCourse->progress }}">
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label for="course" class="mb-2">دوره :</label>
+                        <select name="course_id" id="course" class="w-full bg-[#F9F9F9] py-3 pr-5 rounded-[10px]">
+                            @foreach ($courses as $course)
+                                <option value="{{ $course->id }}"
+                                    @if ($course->id == $userCourse->course_id) {{ 'selected' }} @endif>{{ $course->title }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="text-center mt-4">
+                    <button type="submit"
+                        class="py-3 px-10 rounded-[10px] bg-[#1B84FF] hover:bg-[#056EE9] text-white cursor-pointer">ثبت</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
